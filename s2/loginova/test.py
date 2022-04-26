9d0d52ed2122edaa87bb02872a34f07f7a5915ef
Andrey Drozdov <turdus@merul.space>
2022-04-26 23:38:53 +0300

    Loginova m3, m5 solved

--- s2/loginova/test.py
+++ s2/loginova/test.py
@@ -1,31 +1,5 @@
-def validate_input(_function: function) -> function:
-    def wrapped(_test_input, _err_msg):
-        __result = _function(_test_input, _err_msg)
+test_dict = {
+    "aaa": "bbb"
+}
 
-    return wrapped
-
-
-@validate_input
-def get_input(_test_input: str = None, _err_msg: str = None):
-    
-    pass
-
-
-
-
-"""
-    def print_is_num_in_range(_number: int, _range: list) -> None:
-        '''Выводит в консоль сообщение с информацией о том,
-        находится ли введённое пользователем число в диапозоне от -1 до 17'''
-        print(f"{_number}{' ' if _number in _range else ' НЕ '}находится в диапозоне от -1 до 17")
-
-
-    if __name__ == "__main__":
-        # если данный модуль является основным, то будут вызваны тесты
-        tests = {
-            "ыякесмк": True
-        }
-        for test, answer in tests.items():
-            print_is_num_in_range(get_input(test), range(-1, 17))
-        
-"""
+print({v: k for k, v in test_dict.items()})
