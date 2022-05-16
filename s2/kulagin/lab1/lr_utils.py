64db59532e50ea75b50088cd3da04bd57b1bd36b
Andrey Drozdov <turdus@merul.space>
2022-05-16 09:51:51 +0300

    Loginova: modules has been addes

---
+++ s2/kulagin/lab1/lr_utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import h5py
+    
+    
+def load_dataset():
+    train_dataset = h5py.File('./datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # признаки для обучения
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # метки для обучения
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # признаки для теста
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # метки для теста
+
+    classes = np.array(test_dataset["list_classes"][:]) # список классов
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
--- s2/loginova/test.py
+++ s2/loginova/test.py
@@ -1,5 +1,14 @@
-test_dict = {
-    "aaa": "bbb"
+# Python program to print the data
+bingo_card = {
+    "B": [12, 9, 14, 7, 8],
+    "I": [17, 21, 28, 19, 29],
+    "N": [34, 33, 34, 31, 39],
+    "G": [52, 49, 59, 51, 54],
+    "O": [65, 75, 68, 69, 63]
 }
 
-print({v: k for k, v in test_dict.items()})
+scheme = "{:<5} {:<5} {:<5} {:<5} {:<5}"
+print(scheme.format("B", "I", "N", "G", "O"))
+for nums in bingo_card.values():
+    n1, n2, n3, n4, n5 = nums
+    print(scheme.format(n1, n2, n3, n4, n5))
